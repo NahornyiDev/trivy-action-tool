74b65d18ef89ad42a72fdc55e2e619c83de3c7b9
NahornyiDev <blackpazific@icloud.com>
2022-03-21 09:55:22 +0200

    Initial commit

---
+++ src/main/java/com/example/trivyactiontool/TrivyActionToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.trivyactiontool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrivyActionToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrivyActionToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trivyactiontool/TrivyActionToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trivyactiontool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrivyActionToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
